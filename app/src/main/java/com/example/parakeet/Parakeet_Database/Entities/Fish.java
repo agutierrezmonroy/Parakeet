f99ab6970ef922987732d1bb1b4006bf9cef280c
cofries <conrad@wildirisdiscovery.com>
2025-12-04 11:12:35 -0800

    Added "edible" to Fish database.

--- app/src/main/java/com/example/parakeet/Parakeet_Database/Entities/Fish.java
+++ app/src/main/java/com/example/parakeet/Parakeet_Database/Entities/Fish.java
@@ -8,5 +8,13 @@ public class Fish {
     @PrimaryKey(autoGenerate = true)
     private int id;
 
+    private boolean edible;
 
+    public boolean isEdible() {
+        return edible;
+    }
+
+    public void setEdible(boolean edible) {
+        this.edible = edible;
+    }
 }
